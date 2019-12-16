4d17eb7c6189e1672fa9bca2134266f052f57b82
Niall Murphy <niallmurphywit@gmail.com>
2019-12-16 13:54:22 +0000

    updated PostSchema

--- tftNode/api/posts/postsModel.js
+++ tftNode/api/posts/postsModel.js
@@ -10,7 +10,7 @@ const CommentSchema = new Schema({
  const PostSchema = new Schema({
    title: {type: String, required: true},
    link: {type: String, optional: true},
-   username: {type: String, required: true},
+   author: {type: String, required: true},
    comments: [CommentSchema],
    upvotes: {type: Number, min: 0, max: 100, default: 0},
  });
